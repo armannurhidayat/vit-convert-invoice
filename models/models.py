682ad1e212d559e41ae963950f2a6d3d7559cd7e
Ferdiansah <ferdiayana@gmail.com>
2019-11-27 14:06:49 +0700

    update functions V3

--- models/models.py
+++ models/models.py
@@ -2,8 +2,7 @@
 
 from odoo import models, fields, api
 import time
-import logging
-_logger = logging.getLogger(__name__)
+
 
 class Import(models.Model):
     _name = 'convert_invoice.convert_invoice'
@@ -23,11 +22,11 @@ class Import(models.Model):
     )
     tgl_pencairan = fields.Date(
         string='Tanggal Pencairan',
-        default=lambda self:time.strftime("%Y-%m-%d")
+        default=lambda self: time.strftime("%Y-%m-%d")
     )
     priode_pinjaman = fields.Date(
         string='Priode Pinjaman',
-        default=lambda self:time.strftime("%Y-%m-%d")
+        default=lambda self: time.strftime("%Y-%m-%d")
     )
     mulai_pembayaran = fields.Integer(
         string='Mulai Pembayaran Pinjaman (Bulan)',
@@ -55,40 +54,44 @@ class Import(models.Model):
     )
     tanggal = fields.Date(
         string='Tanggal',
-        default=lambda self:time.strftime("%Y-%m-%d")
+        default=lambda self: time.strftime("%Y-%m-%d")
     )
     is_invoice = fields.Boolean(
         string='Is Invoice',
     )
 
     @api.model
-    def convert_invoice(self, values): # Nama function di ambil dari name button
+    def convert_invoice(self, values):
         # Mengambil Object
         object_convert = self.env['convert_invoice.convert_invoice']
         object_invoice = self.env['account.invoice']
         object_partner = self.env['res.partner']
-        object_account = self.env['account.account'] # COA
+        object_account = self.env['account.account']
         object_product = self.env['product.product']
 
-        # Search record pada object berdasarkan status yang open dan is_invoice yang false
-        records = object_convert.search([('status', '=', 'OPEN'),('is_invoice', '=', False)]) # kondisi search
+        # Search record pada object
+        records = object_convert.search([('status', '=', 'OPEN'), ('is_invoice', '=', False)])  # kondisi search
 
         for record in records:
             partner_id = object_partner.search([('name', '=', record.name)])
             account_id = object_account.search([('code', '=', '101200')])
-            product_id = object_product.search([('name', '=', 'bunga cicilan')])
-            
-            invoice_line_ids = [(0,0,{
-                'product_id' : product_id.id, # product_id dari variabel search object diatas
-                'name'       : 'cicilan',
-                'quantity'   : 1,
-                'price_unit' : record.pokok,
-                'account_id' : account_id.id,
+            product_id = object_product.search([('name', '=', 'PokokCicilan')])
+
+
+            date_invoice = record.periode
+
+            invoice_line_ids = [(0, 0, {
+                'product_id': product_id.id,  # product_id dari variabel search object
+                'name': 'cicilan',
+                'quantity': 1,
+                'price_unit': record.pokok,
+                'account_id': account_id.id,
             })]
 
             data = {
-                'partner_id'        : partner_id.id,
-                'date_invoice'      : record.periode,
-                'invoice_line_ids'  : invoice_line_ids,
+                'partner_id': partner_id.id,
+                'date_invoice': date_invoice,
+                'invoice_line_ids': invoice_line_ids
             }
             object_invoice.create(data)
+            record.is_invoice = True
